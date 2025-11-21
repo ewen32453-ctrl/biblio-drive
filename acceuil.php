1e74837c01c6baad7d6fb0785c45ef77816da49f
ewen32453-ctrl <ewen32453@gmail.com>
2025-11-21 17:22:08 +0100

    carrousel

---
+++ acceuil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+	<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-9">
+					barre de recherche (include)
+			</div>
+			<div class="col-sm-3">
+					<img src="chateau.jpg"  >					
+			</div>
+		</div>
+		<div class="row">
+		   <div class="col-sm-9">
+					carroussel / résultat de la recherche / pages d'admin (ajout d'un livre)
+			</div>
+			<div class="col-sm-3">
+					formulaire de connexion / profil connecté (include)
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
+	
